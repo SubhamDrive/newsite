4e9f70932a2d959d9dbdf0496ecb558d8cc2c8da
subham <subham@driveu.in>
2020-04-04 17:23:51 +0530

    Changes urls,views and make migrations

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.http import HttpResponse
 
-from django.shortcuts import render
 
 # Create your views here.
+#it will accept the request and send an http reponse.
+
+def index(request):
+    return HttpResponse("<h>This is the books homepage</h>")
