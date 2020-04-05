31e1927c039cf7178decdad0235dcc20bb5deaf4
subham <subham@driveu.in>
2020-04-05 16:57:25 +0530

    used render function for passing request

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,21 +1,14 @@
 from django.http import HttpResponse
 from .models import Book
-from django.template import loader
+from django.shortcuts import render
 
+#render has inbuilt Httpresponse so insteed of passing request in hhtp directly pass in render
 def index(request):
     all_books = Book.objects.all()
-    template = loader.get_template('books/index.html')
     context={
-        'all_books':all_books
+        'all_books': all_books
     }
-
-    """html=''
-    for book in all_books:
-        url = '/books/' + str(book.id) + "/"
-        html+= '<a href="'+ url +'">' + str(book.Name) + '</a><br>'
-    """
-    return HttpResponse(template.render(context,request))
-
+    return render(request,'books/index.html',context)
 
 def detail(request,book_id):
     return HttpResponse("<h2>Details For Book Id:" +str(book_id)+ "</h2>")
