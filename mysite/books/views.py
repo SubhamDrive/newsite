75c0d3d2b026675fe12145b63babf786de71e0ec
subham <subham@driveu.in>
2020-04-05 23:59:16 +0530

    Make changes in view.py and display editors and teacher name on the index page

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,15 +1,22 @@
-from django.http import HttpResponse
-from .models import Book
+#from django.http import HttpResponse
+from .models import Book,Teacher,Editors
 from django.shortcuts import render
 from django.http import Http404
 
 #render has inbuilt Httpresponse so insteed of passing request in hhtp directly pass in render
+
 def index(request):
     all_books = Book.objects.all()
+    all_teacher = Teacher.objects.all()
+    all_editors = Editors.objects.all()
     context={
-        'all_books': all_books
+        'all_books': all_books,
+        'all_teacher':all_teacher,
+        'all_editors':all_editors
     }
     return render(request,'books/index.html',context)
+
+
 
 def detail(request,book_id):
     try:
