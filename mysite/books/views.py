37f2f079417e98ef5418c4e17d348430e8b82767
subham <subham@driveu.in>
2020-04-05 17:15:05 +0530

    Added detail.html page in book app

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from .models import Book
 from django.shortcuts import render
+from django.http import Http404
 
 #render has inbuilt Httpresponse so insteed of passing request in hhtp directly pass in render
 def index(request):
@@ -11,7 +12,17 @@ def index(request):
     return render(request,'books/index.html',context)
 
 def detail(request,book_id):
-    return HttpResponse("<h2>Details For Book Id:" +str(book_id)+ "</h2>")
+    try:
+        book = Book.object.get(id=book_id)
+    except Book.DoesNotExist:
+        raise Http404("This Book Dose Not Exist")
+
+    context={
+        'book':book
+    }
+
+    return render(request,'books/detail.html',book)
+    #return HttpResponse("<h2>Details For Book Id:" +str(book_id)+ "</h2>")
 
 def byName(request,book_name):
     return HttpResponse("<h2>Details For Book_name: " +str(book_name)+ "</h2>")
