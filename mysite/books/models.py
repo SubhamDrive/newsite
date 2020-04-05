f98123c943f66f925f5c91ab3a25801aea3234e5
subham <subham@driveu.in>
2020-04-05 20:45:34 +0530

    Added new model Teacher

--- mysite/books/models.py
+++ mysite/books/models.py
@@ -10,3 +10,11 @@ class Book(models.Model):
     Price = models.CharField(max_length=100)
     Type = models.CharField(max_length=100)
 
+
+class Teacher(models.Model):
+    def __str__(self):
+        return self.Name + "--" + self.Age + "--" + self.City
+
+    Name = models.CharField(max_length=100)
+    Age = models.CharField(max_length=10)
+    City = models.CharField(max_length=50)
